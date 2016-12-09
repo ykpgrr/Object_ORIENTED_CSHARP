20be750ab6508c763571791f6ba088394a54798a
Yakup Görür <gorury@itu.edu.tr>
2016-12-09 20:32:39 +0200

    Add files via upload

---
+++ Lesson-Codes/11.hafta_/harf_sayici/11.hafta/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _11.hafta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("sw.txt", Encoding.GetEncoding("iso-8859-9"));
+            string text = sr.ReadToEnd();
+            string[] parsedText = parseText(text); ;
+            int TotalCounter = 0;
+            for (int i = 0; i < parsedText.Length; i++)
+            {
+                if (parsedText[i].Trim().Length!=0)
+                {
+                    char[] temp = parsedText[i].ToCharArray();
+                    int wordCount = temp.Length;
+                    Console.WriteLine("{0,15} {1,3}", parsedText[i], wordCount);
+                    TotalCounter += wordCount;
+                 
+
+                }
+            }
+            Console.WriteLine("total words: " + Convert.ToString(TotalCounter));
+            Finish();
+        }
+
+        private static string[] parseText(string text)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9\\s]|[\t\n\r]");
+            return rgx.Replace(text, "").Split(' ');
+        }
+
+        private static void Finish()
+        {
+            Console.WriteLine("exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson-Codes/11.hafta_/kelime_sayici/kelime_sayici/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kelime_sayici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("data.txt", Encoding.GetEncoding("iso-8859-9"));
+            string[] splitted = data.Split(' ');
+            for (int i = 0; i < splitted.Length; i++)
+            {
+                splitted[i] = WordNormalizer(splitted[i]);       //.Trim(',', ' ', '-', ';', ':', '(', ')').ToLower(); silmeye kıyamadım gereksiz aslında burdaki yorum satırında yazan kodlar
+            }
+            Dictionary<string, int> returnValue = new Dictionary<string, int>();
+            for (int i = 0; i < splitted.Length; i++)
+            {
+                MyDictionaryAdd(returnValue, splitted[i]);
+            }
+            returnValue = returnValue.OrderBy(i => i.Key).ToDictionary(mc => mc.Key, mc => mc.Value);
+
+            string[] content = new string[returnValue.Count + 1];
+            for (int i = 0; i < returnValue.Count; i++)
+            {
+                content[i] = returnValue.ElementAt(i).Key + " " + returnValue.ElementAt(i).Value;
+            }
+            content[returnValue.Count] = string.Format("{0} {1} {2} {3}", "total word count=",returnValue.Sum(item => item.Value),"total distinct word count",returnValue.Count);
+
+            File.WriteAllLines("log.txt", content);
+
+        }
+
+        private static string WordNormalizer(string v)
+        {
+            string normalizedWord = string.Empty;
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (Char.IsLetter(v[i]))
+                {
+                    normalizedWord += v[i];
+                }
+            }
+            return normalizedWord.ToLower();
+        }
+
+
+        private static void MyDictionaryAdd(Dictionary<string,int> returnValue, string v)
+        {
+            if (string.IsNullOrWhiteSpace(v))
+            {
+                return;   
+            }
+            for (int i = 0; i < returnValue.Count; i++)
+            {
+             if (returnValue.ElementAt(i).Key==v)
+                {
+                    var tempElement = returnValue.ElementAt(i);
+                    returnValue.Remove(returnValue.ElementAt(i).Key);
+
+                    returnValue.Add(tempElement.Key, tempElement.Value + 1);
+                    return;
+                }
+            }
+            returnValue.Add(v, 1);
+            return;
+        }
+    }
+}
