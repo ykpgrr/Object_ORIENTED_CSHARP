2a4dbadfd9b96513ef4ebf0e68b951044a88d05f
Yakup Görür <gorury@itu.edu.tr>
2016-12-10 20:24:43 +0200

    Add files via upload

---
+++ 2015-2016-midterm-CSS/Question_2_2015_2016_Object_Oriented_Midterm/Question_3_2015_2016_Object_Oriented_Midterm/FileHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Yakup Görür 
+//Istanbul Technical University Electronic and Communication Engineering
+//https://github.com/ykpgrr
+//https://tr.linkedin.com/in/ykpgrr
+
+//Student study guide	~QD253.2 	.S632 	2011	~New York, 	NY. : McGraw-Hill Higher Education , c2011.
+//Organic chemistry	    ~QD253.2   	.S63  	2011	~New York, 	NY  : McGraw-Hill 		   , c2011.
+//Organic chemistry	    ~QD253.2 	.S63 	2011	~New York, 	NY  : McGraw-Hill		   , c2011.
+//Student study guide	~QD253.2 	.S632   2011	~New York, 	NY. : McGraw-Hill Higher Education , c2011.
+//Student study guide	~QD253.2 	.S632   2011	~New York, 	NY. : McGraw-Hill Higher Education , c2011.
+
+
+namespace Question_2_2015_2016_Object_Oriented_Midterm
+{
+    class FileHelper
+    {
+        private string FileName;
+
+        public FileHelper(string fileName)
+        {
+            FileName = fileName;
+            File.Open(FileName, FileMode.OpenOrCreate).Close();
+
+        }
+
+        public string[] ReadAll()
+        {
+            
+            string[] lines = File.ReadAllLines(FileName);
+       
+            return lines;
+        }
+    }
+}
---
+++ 2015-2016-midterm-CSS/Question_2_2015_2016_Object_Oriented_Midterm/Question_3_2015_2016_Object_Oriented_Midterm/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Yakup Görür 
+//Istanbul Technical University Electronic and Communication Engineering
+//https://github.com/ykpgrr
+//https://tr.linkedin.com/in/ykpgrr
+
+
+
+
+//It is not completed!
+
+
+namespace Question_2_2015_2016_Object_Oriented_Midterm
+{
+    class Program
+    {
+
+        private static string FileName = "C:/Users/ykpgrr_windows/Desktop/Library.txt"; //File adress
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> myIndex = new Dictionary<int, int>();
+
+            FileHelper filehelper = new FileHelper(FileName);
+            var books = filehelper.ReadAll();
+
+            while (true)
+            {
+
+
+                Console.WriteLine("Aradığınız kitap hakkında herhangi bir bilgi:");
+
+                string word = Console.ReadLine();
+                string[] splitted_word = word.Split(' ');
+               
+                int name_lenght = splitted_word.Length;
+
+                for (int i = 0; i < splitted_word.Length; i++)
+                {
+                    
+                    find_it(splitted_word[i], books, myIndex);
+                }
+
+                for (int i = 0; i < myIndex.Count; i++)
+                {
+         
+                    if (myIndex.ElementAt(i).Value >= name_lenght)
+                    {
+                        Console.WriteLine("{0}.  {1}", myIndex.ElementAt(i).Key, books[myIndex.ElementAt(i).Key]);
+                    }
+
+                }
+
+            }
+
+
+        }
+
+        private static void find_it(string word, string[] books, Dictionary<int, int> myIndex)
+        {
+            word=word.ToLower();
+
+            for (int j = 0; j < books.Length; j++)
+            {
+                string[] splitted = books[j].Split(' ', '~');
+                
+                for (int i = 0; i < splitted.Length; i++)
+                {
+                    
+                    splitted[i] = splitted[i].ToLower();
+                    if (splitted[i].Contains(word) == true)
+                    {
+                        Console.WriteLine(splitted[i] + " ****** " + word);
+                        if (myIndex.ContainsKey(j))
+                            myIndex[j]++;
+                        else
+                            myIndex.Add(j, 1);
+
+                    }
+                }
+
+
+
+            }
+        }
+    }
+}
